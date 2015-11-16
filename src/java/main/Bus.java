7f4ef07c6ea1455de5ca952f87b8150f072ba9b8
Ivo <Ivo@172.17.72.204>
2015-11-16 14:09:25 +0100

    Changed bus and added classes.

--- src/java/main/Bus.java
+++ src/java/main/Bus.java
@@ -1,4 +1,41 @@
 package main;
 
 public class Bus {
+	private String acquisitionDate;
+	private String saleDate;
+	private int fleetNumber;
+	private int numOfPassengers;
+	private BusType busType;
+	private boolean onRoute;
+	private boolean atStop;
+	
+	public Bus(String acquisitiondate, int fleetnumber, BusType bustype) {
+		this.acquisitionDate = acquisitiondate;
+		this.fleetNumber = fleetnumber;
+		this.busType = bustype;
+	}
+	
+	public void ArrivesAtStop() {
+		this.atStop = true;
+	}
+	
+	public void LeavesStop() {
+		this.atStop = false;
+	}
+	
+	public void StartRoute() {
+		this.onRoute = true;
+	}
+	
+	public void EndRoute() {
+		this.onRoute = false;
+	}
+	
+	public void Sold() {
+	}
+	
+	public void AllocatedType() {
+	}
+	
+	
 }
