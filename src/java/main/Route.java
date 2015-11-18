f162ad2a7970586132e8aa3bf91c2bc837c1deb7
Ivo <Ivo@172.17.73.37>
2015-11-18 11:32:53 +0100

    Revert emptying classes Route and TestBusCrowdedness

--- src/java/main/Route.java
+++ src/java/main/Route.java
@@ -1,4 +1,29 @@
 package main;
 
-public class Route{
+import java.util.ArrayList;
+
+public class Route {
+	private final int number;
+	private final String description;
+	ArrayList<BusStop> stops = new ArrayList<BusStop>();
+	
+	public Route(int number, String description){
+		stops = new ArrayList<BusStop>();
+		this.number = number;
+		this.description = description;
+	}
+
+	public void addStop(BusStop stop) {
+		stops.add(stop);
+		//For Testing whether entries are stored in ArrayList stops
+		for (int i=0; i<stops.size(); i++){
+			System.out.print(stops.get(i).getID() + " ");
+			System.out.print(stops.get(i).getName() + " ");
+			System.out.println(stops.get(i).getLocation() + " ");
+			}
+		System.out.println("End of list");
+		System.out.println("number of entries in ArrayList route for this route: " + stops.size());
+		System.out.println("");
+		//end test
+	}
 }
---
+++ src/java/main/TestBusCrowdedness.java
@@ -0,0 +1,30 @@
+package main;
+
+public class TestBusCrowdedness {
+
+	public static void main(String[] args) {
+		BusType typevolvo = new BusType("Volvo", "B100", 48, 64);
+		BusType typescania = new BusType("Scania", "C50", 52, 48);
+		BusType typedaf = new BusType("DAF", "A150", 60, 30);
+		
+		Bus bus1 = new Bus(1234, typevolvo);
+		Bus bus2 = new Bus(1235, typedaf);
+		
+		BusStop stop1 = new BusStop(1, "University", "Uni");
+		BusStop stop2 = new BusStop(2, "City Centre", "Centre");
+		BusStop stop3 = new BusStop(3, "Stadium", "Stadium");
+		BusStop stop4 = new BusStop(4, "Nordkraft", "NK");
+		
+		Route route1 = new Route(1, "University-Stadium");
+		Route route2 = new Route(2, "University-Centre");
+		Route route3 = new Route(3, "University-Nordkraft");
+		
+		route1.addStop(stop1);
+		route1.addStop(stop4);
+		route1.addStop(stop3);
+		
+		route2.addStop(stop1);
+		route2.addStop(stop2);
+	}
+}
+
