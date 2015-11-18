c08e6fb6210644a9d3fad7e20581b2584044dedc
janusalarsen <janusalarsen@h489.aau1x-2.klient.slv.site.aau.dk>
2015-11-18 11:10:51 +0100

    I added some empty methods in RouteTimetable

--- src/java/main/Route.java
+++ src/java/main/Route.java
@@ -1,4 +1,4 @@
 package main;
 
-public class Route {
+public class Route{
 }
--- src/java/main/RouteTimetable.java
+++ src/java/main/RouteTimetable.java
@@ -1,4 +1,21 @@
 package main;
 
-public class RouteTimetable {
+int starttime;
+int endtime
+
+public class RouteTimetable(int starttime, int endtime) {
+	starttime = TimeOfStartTime;
+	endtime = TimeOfEndTime;
+			
 }
+public void routeAdded() {
+	System.out.println("New route added");
+}
+
+public void routeRemoved() {
+	System.out.println("Route removed");
+}
+public void tableTime() {
+	System.out.println("Time Tabled");
+}
+
