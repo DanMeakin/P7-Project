324401fe19b3be572965a706867670a6374a561c
Ivo <3034@ivo>
2015-11-23 15:42:52 +0100

    Created createStop method

--- src/java/main/Stop.java
+++ src/java/main/Stop.java
@@ -1,16 +1,34 @@
 package main;
+import java.util.*;
 
 public class Stop {
 	private final int id;
 	private final String name;
 	private final double latitude;
-  private final double longitude;
+  	private final double longitude;
+	private static List<Stop> allStops = new ArrayList<>();
 	
-	public Stop(int id, String name, double latitude, double longitude) {
+	private Stop(int id, String name, double latitude, double longitude) {
 		this.id = id;
 		this.name = name;
 		this.latitude = latitude;
-		this.longitude = longitude; 
+		this.longitude = longitude;
+		allStops.add(this);
+	}
+
+	public static Stop createStop(int id, String name, double latitude, double longitude){
+		for (int i = 0; i < allStops.size(); i++) {
+			Stop currentStop = allStops.get(i);
+			int currentID = currentStop.getID();
+			if ( id == currentID) {
+				throw new IllegalArgumentException("Stop with this ID already exists!");
+			}
+		}
+		return new Stop(id, name, latitude, longitude);
+	}
+
+	public static void addStopToList(Stop stop){
+		allStops.add(stop);
 	}
 
 	public int getID(){
