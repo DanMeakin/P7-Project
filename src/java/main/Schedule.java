c85fc20859806cdd21e1bdba9192034af75e234a
Ivo <3034@ivo>
2015-11-26 16:13:55 +0100

    added some basic getter and checking methods

--- src/java/main/Schedule.java
+++ src/java/main/Schedule.java
@@ -1,23 +1,46 @@
 package main;
 
-import java.util.Date;
+import java.util.*;
 
 public class Schedule {
 	public static enum DayOptions {
 		WEEKDAYS, SATURDAY, SUNDAY
 	}
+	private Date validFromDate;
+	private Date validToDate;
+	private DayOptions scheduledDays;
+	private List<RouteTimetable> routeTimeTableList = new ArrayList<>();
+	private List<Bus> busRouteTimeTableList = new ArrayList<>();
 
-	private Date validfromdate;
-	private Date validtodate;
-	private ScheduledDays ScheduledDays;
-	
-	public Schedule (Date validfromdate, Date validtodate, ScheduledDays scheduleddays) {
-		this.validfromdate = validfromdate;
-		this.validtodate = validtodate;
-		this.ScheduledDays = scheduleddays;
+
+	public Schedule (Date validFromDate, Date validToDate, DayOptions scheduledDays) {
+		this.validFromDate = validFromDate;
+		this.validToDate = validToDate;
+		this.scheduledDays = scheduledDays;
 	}
-	
-	public void addRouteTimetable(RouteTimetable RouteTimetable) {
-		
+
+	public void addRouteTimetable(RouteTimetable routeTimetable) {
+		this.routeTimeTableList.add(routeTimetable);
+		this.busRouteTimeTableList.add(null);
+	}
+
+	public void addRouteTimetableWithBus(RouteTimetable routeTimetable, Bus bus) {
+		this.routeTimeTableList.add(routeTimetable);
+		this.busRouteTimeTableList.add(bus);
+	}
+
+	public boolean hasRouteTimetable(){
+		if(this.routeTimeTableList.isEmpty()){
+			return false;
+		}
+		return true;
+	}
+
+	//This will not actually test whether a Bus is assigned to a schedule, since the list could be filled with nulls.
+	public boolean hasBus(){
+		if(this.busRouteTimeTableList.isEmpty()){
+			return false;
+		}
+		return true;
 	}
 }
