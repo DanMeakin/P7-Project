df39b1c48e3a81e153fafa94189bd104fd2d1fa5
Dan Meakin <dan@danmeakin.com>
2015-11-26 14:35:03 +0100

    Create static enum for ScheduledDays in Schedule class

--- src/java/main/Schedule.java
+++ src/java/main/Schedule.java
@@ -3,14 +3,13 @@ package main;
 import java.util.Date;
 
 public class Schedule {
+	public static enum ScheduledDays {
+		WEEKDAYS, SATURDAY, SUNDAY
+	}
+
 	private Date validfromdate;
 	private Date validtodate;
 	private ScheduledDays ScheduledDays;
-	
-	public enum ScheduledDays {
-		WEEKDAYS, SATURDAY, SUNDAY
-	
-	}
 	
 	public Schedule (Date validfromdate, Date validtodate, ScheduledDays scheduleddays) {
 		this.validfromdate = validfromdate;
