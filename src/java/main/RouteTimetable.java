99dc6b979627310cca387e3fcb9a2d56bee75b09
Ivo <3034@ivo>
2015-11-26 15:42:39 +0100

    minor edits

--- src/java/main/RouteTimetable.java
+++ src/java/main/RouteTimetable.java
@@ -1,8 +1,4 @@
-package java.main;
-
-
-import main.Route;
-
+package main;
 import java.util.*;
 
 public class RouteTimetable {
@@ -11,37 +7,32 @@ public class RouteTimetable {
     private boolean isRushHour;
     private Schedule schedule;
 
-
-
     public RouteTimetable(Route route, Schedule schedule,int starttime, boolean isrushhour) {
         this.startTime = starttime;
         this.schedule = schedule;
         this.isRushHour = isrushhour;
         this.route = route;
-
-
     }
+
     public int getStartTime(){
         return this.startTime;
-
     }
+
     public boolean isRushHour(){
         return this.isRushHour;
-
     }
+
     public List<Integer> getStopTimes(){
-        List<Integer> actualTime = new ArrayList<>();
+        List<Integer> actualTiming = new ArrayList<>();
         for(int i = 0; i < this.route.getStopTiming(this.isRushHour, true).size(); i++){
-            int cummulativeStopTiming = this.route.getStopTiming(this.isRushHour, true).get(i) + this.startTime;
-            actualTime.add(i, cummulativeStopTiming);
+            int cumulativeStopTiming = this.route.getStopTiming(this.isRushHour, true).get(i) + this.startTime;
+            actualTiming.add(i, cumulativeStopTiming);
         }
-        return actualTime;
-
+        return actualTiming;
     }
+
     public List<Stop> getStops(){
         return this.route.getStops();
-
     }
 
 }
-
