23e9672051d03ceaa9a1166a592392e80af4774d
Ivo <3034@ivo>
2015-11-27 15:52:39 +0100

    fixed boolean methods for checking wether bus/routeTimeTable are associated with schedule

--- src/java/main/Schedule.java
+++ src/java/main/Schedule.java
@@ -10,7 +10,7 @@ public class Schedule {
 	private Date validToDate;
 	private DayOptions scheduledDays;
 	private List<RouteTimetable> routeTimeTableList = new ArrayList<>();
-	private List<Bus> busRouteTimeTableList = new ArrayList<>();
+	private List<Bus> busList = new ArrayList<>();
 
 
 	public Schedule (Date validFromDate, Date validToDate, DayOptions scheduledDays) {
@@ -21,7 +21,12 @@ public class Schedule {
 
 	public void addRouteTimetable(RouteTimetable routeTimetable) {
 		this.routeTimeTableList.add(routeTimetable);
-		this.busRouteTimeTableList.add(null);
+		this.busList.add(null);
+	}
+
+	public void addRouteTimetable(RouteTimetable routeTimetable, Bus bus) {
+		this.routeTimeTableList.add(routeTimetable);
+		this.busList.add(bus);
 	}
 
 	public Bus getAllocatedBus(RouteTimetable routeTimetable){
@@ -29,40 +34,44 @@ public class Schedule {
 		String msg = "Routetimetable" + routeTimetable + "is not added to this schedule";
 		for (i=0; i < routeTimeTableList.size(); i++) {
 			if(routeTimeTableList.get(i).equals(routeTimetable)) {
-				return busRouteTimeTableList.get(i);
+				return busList.get(i);
 			}
 		}
 		throw new UnsupportedOperationException(msg);
 	}
 
-	public RouteTimetable getAllocatedRouteTimetables(Bus bus) {
+	public List<RouteTimetable> getAllocatedRouteTimetables(Bus bus) {
+		List<RouteTimetable> allocatedRouteTimetables = new ArrayList<>();
 		int i;
 		String msg = "Bus" + bus + "is not added to this schedule";
-		for (i=0; i < busRouteTimeTableList.size(); i++) {
-			if(busRouteTimeTableList.get(i).equals(bus)) {
-				return routeTimeTableList.get(i);
+		for (i = 0; i < busList.size(); i++) {
+			if (busList.get(i).equals(bus)) {
+				allocatedRouteTimetables.add(routeTimeTableList.get(i));
 			}
+		}
+		if (!busList.isEmpty()) {
+			return allocatedRouteTimetables;
 		}
 		throw new UnsupportedOperationException(msg);
 	}
 
-	public void addRouteTimetableWithBus(RouteTimetable routeTimetable, Bus bus) {
-		this.routeTimeTableList.add(routeTimetable);
-		this.busRouteTimeTableList.add(bus);
+	public boolean hasRouteTimetable(RouteTimetable routeTimetable){
+		int i;
+		for (i=0; i < routeTimeTableList.size(); i++) {
+			if(routeTimeTableList.get(i).equals(routeTimetable)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
-	public boolean hasRouteTimetable(){
-		if(this.routeTimeTableList.isEmpty()){
-			return false;
+	public boolean hasBus(Bus bus){
+		int i;
+		for (i=0; i < busList.size(); i++) {
+			if(busList.get(i).equals(bus)) {
+				return true;
+			}
 		}
-		return true;
-	}
-
-	//This will not actually test whether a Bus is assigned to a schedule, since the list could be filled with nulls.
-	public boolean hasBus(){
-		if(this.busRouteTimeTableList.isEmpty()){
-			return false;
-		}
-		return true;
+		return false;
 	}
 }
