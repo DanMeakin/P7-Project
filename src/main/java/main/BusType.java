b5146f691d213e0bb74d1f4d9ea554f443faeaf9
Ivo <3034@ivo>
2015-11-29 13:16:15 +0100

    added descriptions to methods and varibles

--- src/main/java/main/BusType.java
+++ src/main/java/main/BusType.java
@@ -1,54 +1,83 @@
 package main;
 
-import java.util.ArrayList;
+import java.util.*;
 
+/**
+ * The BusType class defines bus type objects that hold
+ * several characteristics of the type such as make, model,
+ * capacities etc.
+ * @authors Ivo Hendriks, Janus Avbæk Larsen, Helle Hyllested Larsen, Dan Meakin.
+ */
 public class BusType {
+	/** the make of this bus type  */
 	private final String make;
+	/** the model of this bus type  */
 	private final String model;
+	/** the seated capacity of this bus type  */
 	private final int seatedCapacity;
+	/** the standing capacity of this bus type  */
 	private final int standingCapacity;
-	private static ArrayList<BusType> type = new ArrayList<BusType>();
+	/** a data structure wherin all bus type objects are stored  */
+	private static List<BusType> type = new ArrayList<>();
 
+	/**
+	 * Creates a bus type and add it to the type list.
+	 *
+	 * @param make the make of this bus type.
+	 * @param model the model of this bus type.
+	 * @param seatedcapacity the seated capacity of this bus type.
+	 * @param standingcapacity the standing capacity of this bus type.
+	 */
 	public BusType(String make, String model, int seatedcapacity, int standingcapacity){
 		this.make = make;
 		this.model = model;
 		this.seatedCapacity = seatedcapacity;
 		this.standingCapacity = standingcapacity;
 		type.add(this);
-		/*
-		//For Testing whether entries are stored in ArrayList type
-		for (int i=0; i<type.size(); i++){
-			System.out.print(type.get(i).getMake() + " ");
-			System.out.print(type.get(i).getModel() + " ");
-			System.out.print(type.get(i).getSeatedCapacity() + " ");
-			System.out.print(type.get(i).getStandingCapacity() + " ");
-			System.out.println("");
-			}
-		System.out.println("End of list");
-		System.out.println("number of entries in ArrayList type: " + type.size());
-		System.out.println("");
-		//end test
-		}
-		*/
 	}
 
-	public int getTotalCapacity(){
-		return this.getSeatedCapacity() + this.getStandingCapacity();
-	}
-
+	/**
+	 * Get the make of this bus type.
+	 *
+	 * @return make the make of the bus type.
+	 */
 	public String getMake(){
 		return make;
 	}
-	
+
+	/**
+	 * Get the model of this bus type.
+	 *
+	 * @return model the make of the bus type.
+	 */
 	public String getModel(){
 		return model;
 	}
-	
+
+	/**
+	 * Get the seated capacity of this bus type.
+	 *
+	 * @return seatedCapacity the seated capacity of the bus type.
+	 */
 	public int getSeatedCapacity(){
 		return seatedCapacity;
 	}
-	
+
+	/**
+	 * Get the seated capacity of this bus type.
+	 *
+	 * @return standingCapacity the standing capacity of the bus type.
+	 */
 	public int getStandingCapacity(){
 		return standingCapacity;
+	}
+
+	/**
+	 * Get the total capacity of this bus type.
+	 *
+	 * @return getSeatedCapacity + getStandingCapacity the total capacity of the bus type.
+	 */
+	public int getTotalCapacity(){
+		return this.getSeatedCapacity() + this.getStandingCapacity();
 	}
 }
