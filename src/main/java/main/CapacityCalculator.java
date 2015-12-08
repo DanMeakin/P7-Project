469d13b12c64ef5e3651c61b03b351090b5c4c86
Ivo <3034@ivo>
2015-12-08 15:42:45 +0100

    added some methods in CapacityCalculator

--- src/main/java/main/CapacityCalculator.java
+++ src/main/java/main/CapacityCalculator.java
@@ -1,5 +1,56 @@
 package main;
 
+import java.util.Arrays;
+import java.util.IllegalFormatException;
+import java.util.List;
+import java.util.ArrayList;
+import main.CapacityDataStore;
+import org.apache.commons.math3.stat.regression.SimpleRegression;
+import main.CapacityDataStore;
+
 public class CapacityCalculator {
 
+    private double crowdednessFactor;
+    private double currentCrowdedness;
+
+    public CapacityCalculator() {
+    }
+
+    /*
+    * Relies on org.apache.commons.math3.stat.regression.SimpleRegression
+    */
+    public double calculateCrowdedness(double crowdednessFactor, double currentCrowdedness, RouteTimetable routeTimetable, Stop currentStop, Stop requestedStop) throws IllegalArgumentException {
+        this.crowdednessFactor = crowdednessFactor;
+        this.currentCrowdedness = currentCrowdedness;
+
+
+        List<Double> requestedStopHistoricData = new ArrayList<>(CapacityDataStore.readHistoricRequestedStopCrowdedness(routeTimetable, requestedStop));
+        List<Double> currentStopHistoricData = new ArrayList<>(CapacityDataStore.readHistoricCurrentStopCrowdedness(routeTimetable, currentStop));
+
+        SimpleRegression simpleRegression = new SimpleRegression();
+
+        if (!requestedStopHistoricData.isEmpty() && !currentStopHistoricData.isEmpty() && requestedStopHistoricData.size() == currentStopHistoricData.size()) {
+            for (int i = 0; i < requestedStopHistoricData.size(); i++) {
+                simpleRegression.addData(currentStopHistoricData.get(i), requestedStopHistoricData.get(i));
+            }
+        }
+        return (simpleRegression.predict(currentCrowdedness))*crowdednessFactor;
+    }
+
+    public double calculateCrowdedness(double crowdednessFactor, RouteTimetable rtt, Stop requestedStop) {
+        this.crowdednessFactor = crowdednessFactor;
+
+        CapacityDataStore requestedStopDataReader = new CapacityDataStore();
+
+        List<Double> requestedStopHistoricData = new ArrayList<>(requestedStopDataReader.readHistoricRequestedStopCrowdedness(rtt, requestedStop));
+
+        double averageCrowdedness = 0;
+        if (!requestedStopHistoricData.isEmpty()) {
+            for (Double dataPoint : requestedStopHistoricData) {
+                averageCrowdedness += dataPoint;
+            }
+            return averageCrowdedness / requestedStopHistoricData.size();
+        }
+        return averageCrowdedness*crowdednessFactor;
+    }
 }
