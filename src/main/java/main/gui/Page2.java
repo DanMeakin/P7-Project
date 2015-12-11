8d7b50df65413269cae0f98edc310fd282a0fe15
Janusalarsen <jala12@student.aau.dk>
2015-12-11 13:48:35 +0100

    fized ResultCard class so the values matches the icons and text

--- src/main/java/main/gui/Page2.java
+++ src/main/java/main/gui/Page2.java
@@ -1,17 +1,11 @@
 package main.gui;
 
-import com.sun.corba.se.spi.orbutil.fsm.Guard;
-import com.sun.javafx.scene.control.skin.ScrollBarSkin;
 import org.jdesktop.swingx.border.DropShadowBorder;
 import javax.swing.*;
 import javax.swing.plaf.ScrollBarUI;
 import javax.swing.plaf.basic.BasicScrollBarUI;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.AdjustmentEvent;
-import java.awt.event.AdjustmentListener;
-import java.time.Year;
+
 
 /**
  * Created by janusalarsen on 04/12/2015.
@@ -102,154 +96,18 @@ public class Page2 extends JPanel{
 
         }
 
-        // All Results Container
-        //JPanel allResultsContainer =
-
-        //results
-            //result 0
-        JPanel pResult = new JPanel();
-        pResult.setLayout(new FlowLayout(FlowLayout.LEFT));
-        pResult.setBackground(Color.decode(SECONDARY_COLOR));
-        pResult.setPreferredSize((new Dimension(SCREEN_WIDTH -50, 150)));
-
-        JPanel busIcon = new JPanel();
-        busIcon.setLayout(new GridLayout(0, 1));
-        busIcon.setPreferredSize(new Dimension (100, 100));
-        busIcon.add(new JLabel(new ImageIcon(getClass().getResource("/main/gui/assets/icons/crowdednessCrowded-100x100.png"))));
-        pResult.add(busIcon);
-        busIcon.setBackground(Color.decode(SECONDARY_COLOR));
-
-        JPanel pResultTextContainer = new JPanel();
-        pResultTextContainer.setLayout(new GridLayout(2, 2));
-
-        JLabel pResultNumber = new JLabel("2C                Monday, 30.11.2015");
-        pResultNumber.setFont(h2);
-        pResultNumber.setForeground(Color.BLACK);
-        pResultTextContainer.add(pResultNumber);
-
-
-        JLabel pResultInfo = new JLabel("Depature 15:10, BornHolmsgade (Aalborg), 39 min.");
-        pResultInfo.setFont(h3);
-        pResultInfo.setForeground(Color.BLACK);
-        pResultTextContainer.setBackground(Color.decode(SECONDARY_COLOR));
-        pResultTextContainer.setPreferredSize(new Dimension(300, 150));
-        pResultTextContainer.add(pResultInfo);
-        pResult.add(pResultTextContainer);
-
-            //result1
-        JPanel pResult1 = new JPanel();
-        pResult1.setLayout(new FlowLayout(FlowLayout.LEFT));
-        pResult1.setBackground(Color.decode(SECONDARY_COLOR));
-        pResult1.setPreferredSize((new Dimension(SCREEN_WIDTH -50, 150)));
-
-        JPanel busIcon1 = new JPanel();
-        busIcon1.setLayout(new GridLayout(0, 1));
-        busIcon1.setPreferredSize(new Dimension (100, 100));
-        busIcon1.add(new JLabel(new ImageIcon(getClass().getResource("/main/gui/assets/icons/crowdednessNotCrowded.png"))));
-        pResult1.add(busIcon1);
-        busIcon1.setBackground(Color.decode(SECONDARY_COLOR));
-
-        JPanel pResultTextContainer1 = new JPanel();
-        pResultTextContainer1.setLayout(new GridLayout(2, 2));
-
-        JLabel pResultNumber1 = new JLabel("2B                Monday, 30.11.2015");
-        pResultNumber1.setFont(h2);
-        pResultNumber1.setForeground(Color.BLACK);
-        pResultTextContainer1.add(pResultNumber1);
-
-
-        JLabel pResultInfo1 = new JLabel("Depature 15:25, BornHolmsgade (Aalborg), 45 min.");
-        pResultInfo1.setFont(h3);
-        pResultInfo1.setForeground(Color.BLACK);
-        pResultTextContainer1.setBackground(Color.decode(SECONDARY_COLOR));
-        pResultTextContainer1.setPreferredSize(new Dimension(300, 150));
-        pResultTextContainer1.add(pResultInfo1);
-        pResult1.add(pResultTextContainer1);
-
-            //result 2
-        JPanel pResult2 = new JPanel();
-        pResult2.setLayout(new FlowLayout(FlowLayout.LEFT));
-        pResult2.setBackground(Color.decode(SECONDARY_COLOR));
-        pResult2.setPreferredSize((new Dimension(SCREEN_WIDTH -50, 150)));
-
-        JPanel busIcon2 = new JPanel();
-        busIcon2.setLayout(new GridLayout(0, 1));
-        busIcon2.setPreferredSize(new Dimension (100, 100));
-        busIcon2.add(new JLabel(new ImageIcon(getClass().getResource("/main/gui/assets/icons/crowdednessMediumCrowded.png"))));
-        pResult2.add(busIcon2);
-        busIcon2.setBackground(Color.decode(SECONDARY_COLOR));
-
-        JPanel pResultTextContainer2 = new JPanel();
-        pResultTextContainer2.setLayout(new GridLayout(2, 2));
-
-        JLabel pResultNumber2 = new JLabel("2A                Monday, 30.11.2015");
-        pResultNumber2.setFont(h2);
-        pResultNumber2.setForeground(Color.BLACK);
-        pResultTextContainer2.add(pResultNumber2);
-
-
-        JLabel pResultInfo2 = new JLabel("Depature 15:36, BornHolmsgade (Aalborg), 45 min.");
-        pResultInfo2.setFont(h3);
-        pResultInfo2.setForeground(Color.BLACK);
-        pResultTextContainer2.setBackground(Color.decode(SECONDARY_COLOR));
-        pResultTextContainer2.setPreferredSize(new Dimension(300, 150));
-        pResultTextContainer2.add(pResultInfo2);
-        pResult2.add(pResultTextContainer2);
-
-            //result 3
-        JPanel pResult3 = new JPanel();
-        pResult3.setLayout(new FlowLayout(FlowLayout.LEFT));
-        pResult3.setBackground(Color.decode(SECONDARY_COLOR));
-        pResult3.setPreferredSize((new Dimension(SCREEN_WIDTH -50, 150)));
-
-        JPanel busIcon3 = new JPanel();
-        busIcon3.setLayout(new GridLayout(0, 1));
-        busIcon3.setPreferredSize(new Dimension (100, 100));
-        busIcon3.add(new JLabel(new ImageIcon(getClass().getResource("/main/gui/assets/icons/crowdednessNotCrowded.png"))));
-        pResult3.add(busIcon3);
-        busIcon3.setBackground(Color.decode(SECONDARY_COLOR));
-
-        JPanel pResultTextContainer3 = new JPanel();
-        pResultTextContainer3.setLayout(new GridLayout(2, 2));
-
-        JLabel pResultNumber3 = new JLabel("2E                Monday, 30.11.2015");
-        pResultNumber3.setFont(h2);
-        pResultNumber3.setForeground(Color.BLACK);
-        pResultTextContainer3.add(pResultNumber3);
-
-
-        JLabel pResultInfo3 = new JLabel("Depature 15:54, BornHolmsgade (Aalborg), 54 min.");
-        pResultInfo3.setFont(h3);
-        pResultInfo3.setForeground(Color.BLACK);
-        pResultTextContainer3.setBackground(Color.decode(SECONDARY_COLOR));
-        pResultTextContainer3.setPreferredSize(new Dimension(300, 150));
-        pResultTextContainer3.add(pResultInfo3);
-        pResult3.add(pResultTextContainer3);
-
-        //Divider
-        JPanel pDivider = new JPanel();
-        pDivider.setPreferredSize(new Dimension(SCREEN_WIDTH, 15));
-
-        JPanel pDivider1 = new JPanel();
-        pDivider1.setPreferredSize(new Dimension(SCREEN_WIDTH, 15));
-
-        JPanel pDivider2 = new JPanel();
-        pDivider2.setPreferredSize(new Dimension(SCREEN_WIDTH, 15));
-
         //Last adds
         scrollPane.setViewportView(pContent);
         this.add(pSearchContainer);
         this.add(scrollPane);
 
-        pContent.add(pResult);
-        pContent.add(pDivider);
-        pContent.add(pResult1);
-        pContent.add(pDivider1);
-        pContent.add(pResult2);
-        pContent.add(pDivider2);
-        pContent.add(pResult3);
-
-        pContent.add(new ResultCard());
+        pContent.add(new ResultCard(10));
+        pContent.add(new ResultCard(35));
+        pContent.add(new ResultCard(80));
+        pContent.add(new ResultCard(30));
+        pContent.add(new ResultCard(10));
+        pContent.add(new ResultCard(80));
+        pContent.add(new ResultCard(60));
 
         setVisible(true);
     }
--- src/main/java/main/gui/ResultCard.java
+++ src/main/java/main/gui/ResultCard.java
@@ -1,46 +1,117 @@
 package main.gui;
 
 import javax.swing.*;
-import javax.xml.transform.Result;
 import java.awt.*;
 
 /**
  * Created by janusalarsen on 10/12/2015.
  */
 public class ResultCard extends JPanel {
-    private String name;
     private final int CARD_HEIGHT = 150;
     private final int CARD_WIDTH = 430;
     private final String SECONDARY_COLOR = "#FAFAFA";
     private final Font h2 = new Font("Roboto", Font.PLAIN, 18);
     private final Font h3 = new Font("Roboto", Font.PLAIN, 14);
+    private ImageIcon green = new ImageIcon(getClass().getResource("/main/gui/assets/icons/crowdednessNotCrowded.png"));
+    private ImageIcon yellow = new ImageIcon(getClass().getResource("/main/gui/assets/icons/crowdednessMediumCrowded.png"));
+    private ImageIcon red = new ImageIcon(getClass().getResource("/main/gui/assets/icons/crowdednessCrowded-100x100.png"));
 
-    public ResultCard(String name) {
+
+    public ResultCard(int crowdedness) {
         super();
         this.setLayout(new FlowLayout(FlowLayout.LEFT));
         this.setPreferredSize(new Dimension(CARD_WIDTH, CARD_HEIGHT));
         this.setBackground(Color.decode(SECONDARY_COLOR));
         setVisible(true);
 
+        JPanel busIcon = new JPanel();
+        busIcon.setLayout(new GridLayout(0, 1));
+        busIcon.setPreferredSize(new Dimension (100, 100));
+
+        JLabel busIconLabel = new JLabel();
+        this.add(busIcon);
+
+        busIconLabel.setIcon(crowdedness(crowdedness));
+        busIcon.add(busIconLabel);
+
+        busIcon.setBackground(Color.decode(SECONDARY_COLOR));
+
+       /* JPanel pResultTextContainer = new JPanel();
+        pResultTextContainer.setLayout(new GridLayout(2, 2));
+
+        JLabel pResultNumber = new JLabel("2C                Monday, 30.11.2015");
+        pResultNumber.setFont(h2);
+        pResultNumber.setForeground(Color.BLACK);
+        pResultTextContainer.add(pResultNumber);
+        JLabel pResultInfo = new JLabel("Depature 15:10, BornHolmsgade (Aalborg), 39 min.");
+        pResultInfo.setFont(h3);
+        pResultInfo.setForeground(Color.BLACK);
+        pResultTextContainer.setBackground(Color.decode(SECONDARY_COLOR));
+        pResultTextContainer.setPreferredSize(new Dimension(300, 150));
+        pResultTextContainer.add(pResultInfo);
+        this.add(pResultTextContainer);*/
+
         JPanel pDivider = new JPanel();
         pDivider.setPreferredSize(new Dimension(CARD_WIDTH, 15));
+
         }
+        public ImageIcon crowdedness(int crowdedness){
 
-        private JPanel getCrowdedness(){
-            JPanel pCardContainer = new JPanel();
-            pCardContainer.setLayout(new FlowLayout(FlowLayout.LEFT));
-            pCardContainer.setPreferredSize(new Dimension(CARD_WIDTH, CARD_HEIGHT));
-            pCardContainer.setBackground(Color.decode(SECONDARY_COLOR));
+        ImageIcon crowdednessIcon;
 
-            if(String="unCrowded") {
-                return ResultCard(unCrowded);
-            }else if (String = mediumCrowded) {
-                return ResultCard(mediumCrowded);
-            }
-            }else if (String = "crowded"){
-                return ResultCard(crowded);
-            }
+        if (crowdedness >= 75){
+            crowdednessIcon = red;
+            JPanel pResultTextContainer = new JPanel();
 
-            return pCardContainer;
+            pResultTextContainer.setLayout(new GridLayout(2, 2));
+            JLabel pResultNumber = new JLabel("2C                Monday, 30.11.2015");
+            pResultNumber.setFont(h2);
+            pResultNumber.setForeground(Color.BLACK);
+            pResultTextContainer.add(pResultNumber);
+            JLabel pResultInfo = new JLabel("Depature 15:10, BornHolmsgade (Aalborg), 39 min.");
+            pResultInfo.setFont(h3);
+            pResultInfo.setForeground(Color.BLACK);
+            pResultTextContainer.setBackground(Color.decode(SECONDARY_COLOR));
+            pResultTextContainer.setPreferredSize(new Dimension(300, 150));
+            pResultTextContainer.add(pResultInfo);
+            this.add(pResultTextContainer);
+        }else if (crowdedness >= 35){
+            crowdednessIcon = yellow;
+
+            JPanel pResultTextContainer1 = new JPanel();
+            pResultTextContainer1.setLayout(new GridLayout(2, 2));
+            JLabel pResultNumber1 = new JLabel("2B                Monday, 30.11.2015");
+            pResultNumber1.setFont(h2);
+            pResultNumber1.setForeground(Color.BLACK);
+            pResultTextContainer1.add(pResultNumber1);
+            JLabel pResultInfo1 = new JLabel("Depature 15:15, BornHolmsgade (Aalborg), 40 min.");
+            pResultInfo1.setFont(h3);
+            pResultInfo1.setForeground(Color.BLACK);
+            pResultTextContainer1.setBackground(Color.decode(SECONDARY_COLOR));
+            pResultTextContainer1.setPreferredSize(new Dimension(300, 150));
+            pResultTextContainer1.add(pResultInfo1);
+            this.add(pResultTextContainer1);
+        }else {
+            crowdednessIcon = green;
+
+            JPanel pResultTextContainer2 = new JPanel();
+            pResultTextContainer2.setLayout(new GridLayout(2, 2));
+            JLabel pResultNumber2 = new JLabel("2E                Monday, 30.11.2015");
+            pResultNumber2.setFont(h2);
+            pResultNumber2.setForeground(Color.BLACK);
+            pResultTextContainer2.add(pResultNumber2);
+            JLabel pResultInfo = new JLabel("Depature 15:20, BornHolmsgade (Aalborg), 45 min.");
+            pResultInfo.setFont(h3);
+            pResultInfo.setForeground(Color.BLACK);
+            pResultTextContainer2.setBackground(Color.decode(SECONDARY_COLOR));
+            pResultTextContainer2.setPreferredSize(new Dimension(300, 150));
+            pResultTextContainer2.add(pResultInfo);
+            this.add(pResultTextContainer2);
         }
-}
+        return crowdednessIcon;
+        }
+    }
+
+
+
+
