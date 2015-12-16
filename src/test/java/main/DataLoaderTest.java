079701cf398f474cce5d3add26900f6f17cb374f
Dan Meakin <dan@danmeakin.com>
2015-12-16 22:16:54 +0100

    Add Route & Stop data; DataLoader test

---
+++ src/test/java/main/DataLoaderTest.java
@@ -0,0 +1,17 @@
+package main;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+/**
+ * This class contains a suite of unit tests for the DataLoader class.
+ */
+public class DataLoaderTest {
+
+  @Test
+  public void testDataLoader() {
+
+  }
+}
